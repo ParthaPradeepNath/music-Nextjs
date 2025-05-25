aac6213dae4beaef12c80ce25cb4cdbe473a49ff
ParthaPradeepNath <parthanath96134@gmail.com>
2025-05-25 10:38:26 +0530

    made ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: { // from where images are coming i am configuring that for optimization
+    domains: ['images.unsplash.com','res.cloudinary.com']
+  }
 };
 
 export default nextConfig;
